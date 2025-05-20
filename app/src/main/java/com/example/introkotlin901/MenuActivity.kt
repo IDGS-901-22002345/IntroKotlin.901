5234aa5ad4a15cbfaabd6da3fcc7b3dd95996376
MaicoLopez <78677@alumnos.utleon.edu.mx>
2025-05-19 21:06:47 -0600

    Ejemplo 2

---
+++ app/src/main/java/com/example/introkotlin901/MenuActivity.kt
@@ -0,0 +1,33 @@
+package com.example.introkotlin901
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.introkotlin901.ejemplo1.SumaActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+
+        val btnSaludo = findViewById<Button>(R.id.btn1)
+
+        btnSaludo.setOnClickListener { navegateToSaludo() }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun navegateToSaludo () {
+        val intent = Intent(this, SumaActivity::class.java)
+        startActivities(arrayOf(intent))
+    }
+}
